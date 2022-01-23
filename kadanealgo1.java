7b37fc76c2da52db3f801f837a5b4540bfe8c9d2
Lareb <lareb131213@gmail.com>
2022-01-23 22:34:59 +0530

    kadane commit

---
+++ kadanealgo1.java
@@ -0,0 +1,38 @@
+class Main{ // yaha class ka naam main krna pada 
+    public  static void   main (String [] args) {
+        int arr[] = {1 , 2, 3 , -4 ,5 , 1 ,-3 , 4};
+        int n = 8;
+        int re = maxSubarraySum(arr,n); // yaha agar bina re k call kr ra tha toh error show ho ra tha
+        System.out.println(re);
+    }
+    public static int maxSubarraySum(int arr[], int n){ // yaha method ka return type void hatana pada 
+        int maxsum = 0;
+        int cursum = 0;
+        int l = 0 ;
+        int m = 0;
+        for ( int i = 0 ; i < arr.length ; i++) {
+            if (arr[i]< 0){
+                ++l;
+                if(l==1){
+                    m = arr[i];
+                }
+                if (arr[i] > m ){
+                    m = arr[i];
+                }
+            }
+            cursum = cursum + arr[i];
+            if (cursum > maxsum) {
+                maxsum = cursum;
+            }
+            if (cursum < 0){
+                cursum = 0;
+            }
+        }
+        if(maxsum == 0 ){
+            return m;
+        }else {
+            return maxsum;
+        }
+    }
+    
+}
