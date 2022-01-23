bc078659bc9b49c8dead5fb2bbffbc39c4c91bcb
Lareb <lareb131213@gmail.com>
2022-01-23 13:30:11 +0530

    8th commit

---
+++ cyclingarray.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Compute {
+    
+    public static void rotate(int arr[], int n)
+    {
+        int temp = arr[n-1];
+        for (int i = n-1; i>= 0 ;i--) {
+        
+            if(i == 0){
+                arr[i] = temp;
+            }else{
+            arr[i] = arr[i-1];
+        }
+        }
+    }
+public static void main(String[] args)
+    {
+        int arr[] = {1 ,2, 2, 4, 5};
+        int n = 5;
+        rotate(arr,n);
+        for ( int e = 0 ; e< n ; e++) {
+            System.out.println(arr[e]);
+        }
+
+    }
+}
